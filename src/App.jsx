5276045bb2558e99399c31eebc0683373b7cbea3
Ömer Fatih <omer@workintech.com.tr>
2025-02-24 11:11:59 +0300

    day 1 updates: first layout, clean files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+import viteLogo from "/vite.svg";
+<img src={viteLogo} className="logo" alt="Vite logo" />
+*/
+import "./reset.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="mainpage-container">
+      <div className="left-column">
+        <h2>Burada dizi listesi olacak</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="center-content">Dizi özeti</div>
+      <div className="right-column">
+        <h2>İzlemek istediklerim</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
